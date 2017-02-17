2d419b4fbf05985047c7f6e272bafaa5854c43a7
lcyisgod <2503978335@qq.com>
2017-02-17 15:24:33 +0800

    Initial Commit

---
+++ MovePlayerTests/MovePlayerTests.m
@@ -0,0 +1,39 @@
+//
+//  MovePlayerTests.m
+//  MovePlayerTests
+//
+//  Created by apple on 2017/2/17.
+//  Copyright © 2017年 apple. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface MovePlayerTests : XCTestCase
+
+@end
+
+@implementation MovePlayerTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
